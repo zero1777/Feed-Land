110fb955a5b9d3c3767b770e9b72b8f931ac26a3
ms00769171 <ms00769171@gmail.com>
2021-12-01 23:16:11 +0800

    fix: indent fix

--- Assets/Scripts/RoleController.cs
+++ Assets/Scripts/RoleController.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.AI; //for NavMeshAgent
-using System;
 
 public class RoleController : MonoBehaviour
 {
