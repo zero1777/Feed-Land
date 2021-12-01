36e4e086d3b13b5ec0711db88e8be0ce2aeb88c1
ShellyChaoo <shelly.chao@iss.nthu.edu.tw>
2021-12-01 21:54:28 +0800

    fix: master branch and feature/cannon-shooting conflict

---
+++ Assets/Scripts/Cannon/BulletHitDragon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHitDragon : MonoBehaviour
+{
+    // Destroy bullet when it hit on dragon
+    void OnCollisionEnter(Collision other) 
+    {
+        if (other.collider.tag.Contains("monster"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
