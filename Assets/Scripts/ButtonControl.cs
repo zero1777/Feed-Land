56f6b85b0f6b9f721135dd2461e468a5ed9ddce2
rainyjonne <rainyjonne@gmail.com>
2021-12-11 19:05:42 +0800

    Feat: main menu related features (play, exit, main music)

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl : MonoBehaviour
+{
+    public AudioSource audioPlayer;
+    public AudioClip clickSound;
+    public GameObject exitCanvas;
+
+    public void PlayButtonSound() {
+        audioPlayer.PlayOneShot(clickSound);
+    }
+
+    public void LoadGameScene() {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ReturnToMenu(){
+        exitCanvas.SetActive(false);
+    }
+
+    public void PrepareToExit() {
+        exitCanvas.SetActive(true);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
