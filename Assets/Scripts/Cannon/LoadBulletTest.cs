cde5a461a5bc8f8068214765b81ad03a810b1a9f
ShellyChaoo <shelly.chao@iss.nthu.edu.tw>
2021-12-19 13:38:13 +0800

    modify cannon demo scene

--- Assets/Scripts/Cannon/LoadBulletTest.cs
+++ Assets/Scripts/Cannon/LoadBulletTest.cs
@@ -7,6 +7,7 @@ public class LoadBulletTest : MonoBehaviour
     public GameObject cannonObject;
     public GameObject redFoodPrefab;
     public GameObject blueFoodPrefab;
+    public GameObject greenFoodPrefab;
 
     // Update is called once per frame
     void Update()
@@ -19,6 +20,11 @@ public class LoadBulletTest : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             cannonObject.GetComponent<CannonSenseShoot>().LoadBullet(blueFoodPrefab);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            cannonObject.GetComponent<CannonSenseShoot>().LoadBullet(greenFoodPrefab);
         }
     }
 }
