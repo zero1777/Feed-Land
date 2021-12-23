df95daf7264bf343d3c6ab44993db7c8b613b655
xtutx <xtutx@xtutx.local>
2021-12-23 17:25:06 +0800

    feat: code modification from code review :)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -19,9 +19,8 @@ public class MapGenerator : MonoBehaviour
     private int minesNum;
     private int mapWidth;
     private int mapHeight;
-    private List<List<int>> paths;
+    private List<List<Vector3>> paths;
     private float offsetY;
-    private int mapIdx;
 
     void Start()
     {
@@ -29,25 +28,23 @@ public class MapGenerator : MonoBehaviour
         mapWidth = 28;
         mapHeight = 14;
         offsetY = 0.5f;
-        mapIdx = 0;
         turretPlaceNum = 6;
-        paths = new List<List<int>>();
+        paths = new List<List<Vector3>>();
 
-        while (mapIdx < mapNum)
+        for (int mapIdx = 0; mapIdx < mapNum; mapIdx++)
         {
             // create ground first
             Vector3 mapPos = new Vector3(mapIdx * mapWidth, 0f, 0f);
             Instantiate(mapPrefab, mapPos, Quaternion.identity);
             // generate path on the map
-            GeneratePath();
+            GeneratePath(new Vector3(-13.5f + mapIdx * mapWidth, 0.5f, -6.5f));
             // generate elements on the map
-            treesNum = Random.Range(minElementNum, maxElementNum + 1);
-            minesNum = Random.Range(minElementNum, maxElementNum + 1);
-            GenerateElement(minesNum, "left", minePrefab);
-            GenerateElement(treesNum, "right", treePrefab);
+            treesNum = Random.Range(minElementNum, maxElementNum);
+            minesNum = Random.Range(minElementNum, maxElementNum);
+            GenerateElement(minesNum, new Vector3(-13.5f + mapIdx * mapWidth, 0.5f, 6.5f), minePrefab);
+            GenerateElement(treesNum, new Vector3(0.5f + mapIdx * mapWidth, 0.5f, 6.5f), treePrefab);
             // generate turretPlace on the map
-            GenerateTurretPlace();
-            mapIdx++;
+            GenerateTurretPlace(new Vector3(-9.5f + mapIdx * mapWidth, 0f, -1.5f));
         }
     }
 
@@ -57,128 +54,87 @@ public class MapGenerator : MonoBehaviour
 
     }
 
-    // assume that treePrefab is placed at the right half of the map
-    // and the minePrefab is placed at the left half of the map 
-    private void GenerateElement(int total, string region, GameObject _object)
+    private void GenerateElement(int total, Vector3 offset, GameObject prefab)
     {
-        List<int> elementIdx = new List<int>();
-        // temporarily set the region size = 14 * 7
-        int regionSize = 98;
+        HashSet<Vector3> elements = new HashSet<Vector3>();
+
         for (int i = 0; i < total; i++)
         {
-            int idx;
+            Vector3 point;
             do
             {
-                idx = Random.Range(0, regionSize);
-            }
-            while (elementIdx.Contains(idx));
-            elementIdx.Add(idx);
+                point = new Vector3(Random.Range(0, 14), 0f, (-1) * Random.Range(0, 7));
+            } while (elements.Contains(point));
+            elements.Add(point);
 
-            // translate the idx to coordinate
-            Vector3 pos = idxToPos(idx, region);
-            Instantiate(_object, pos, Quaternion.identity);
+            Vector3 position = point + offset;
+            Instantiate(prefab, position, Quaternion.identity);
         }
     }
 
-    private Vector3 idxToPos(int idx, string region)
-    {
-        int row = mapWidth / 2;
-        int _x = idx % row;
-        int _z = idx / row;
-        int mapOffset = mapIdx * mapWidth;
-
-        float offsetX, offsetZ;
-        if (region == "left")
-        {
-            offsetX = -13.5f;
-            offsetZ = 6.5f;
-        }
-        else
-        {
-            offsetX = 0.5f;
-            offsetZ = 6.5f;
-        }
-
-        float x = offsetX + _x + mapOffset;
-        float z = offsetZ - _z;
-        return new Vector3(x, offsetY, z);
-    }
-
-    private void GeneratePath()
+    private void GeneratePath(Vector3 offset)
     {
         // First, random each column position
-        List<int> pos = new List<int>();
+        List<int> zPositions = new List<int>();
         int lines = 4;
         for (int i = 0; i < mapWidth - 2; i++)
         {
-            pos.Add(Random.Range(0, lines));
+            zPositions.Add(Random.Range(0, lines));
         }
-        pos.Add(0);
+        zPositions.Add(0);
 
         // direction
         // 1 -> up
         // 0 -> right
         // -1 -> down
         // Next, generate the path according to each column position
-        int currentPos = 0;
-        List<int> path = new List<int>();
-        foreach (var p in pos)
+        int currentZPosition = 0;
+        List<Vector3> path = new List<Vector3>();
+        path.Add(offset);
+        foreach (int p in zPositions)
         {
-            path.Add(0);
-            if (currentPos != p)
+            Vector3 point = new Vector3(1f, 0f, 0f);
+            offset += point;
+            path.Add(offset);
+
+            if (currentZPosition != p)
             {
-                int step = (currentPos > p) ? -1 : 1;
-                for (int i = 0; i < Mathf.Abs(currentPos - p); i++) path.Add(step);
+                int step = (currentZPosition > p) ? -1 : 1;
+                for (int i = 0; i < Mathf.Abs(currentZPosition - p); i++)
+                {
+                    Vector3 vec = (currentZPosition > p) ? new Vector3(0f, 0f, -1) : new Vector3(0f, 0f, 1);
+                    offset += vec;
+                    path.Add(offset);
+                }
             }
-            currentPos = p;
+            currentZPosition = p;
         }
 
         // Finally, load the effect on the determined path
         LoadEffect(path);
         paths.Add(path);
     }
-
-    private void LoadEffect(List<int> path)
+    private void LoadEffect(List<Vector3> path)
     {
-        int mapOffset = mapIdx * mapWidth;
-        float startX = -13.5f + mapOffset;
-        float startZ = -6.5f;
-
-        Vector3 currentPos = new Vector3(startX, offsetY, startZ);
-        Instantiate(pathEffectPrefab, currentPos, Quaternion.identity);
-        foreach (var p in path)
+        foreach (Vector3 p in path)
         {
-            // direction
-            // 1 -> up
-            // 0 -> right
-            // -1 -> down
-            if (p == -1 || p == 1)
-            {
-                currentPos.z += p;
-            }
-            else
-            { // p = 0
-                currentPos.x += 1;
-            }
-            Instantiate(pathEffectPrefab, currentPos, Quaternion.identity);
+            Instantiate(pathEffectPrefab, p, Quaternion.identity);
         }
     }
 
-    public List<int> GetPath(int idx)
+    public List<Vector3> GetPath(int idx)
     {
         return paths[idx];
     }
 
-
-    private void GenerateTurretPlace()
+    private void GenerateTurretPlace(Vector3 offset)
     {
         // for convenience, directly set the constant
-        float x = -9.5f + mapIdx * mapWidth;
-        float z = -1.5f;
         for (int i = 0; i < turretPlaceNum; i++)
         {
-            Vector3 vec = new Vector3(x + i * 4f, 0f, z);
-            Instantiate(turretPlacePrefab, vec, Quaternion.identity);
+            Vector3 point = new Vector3(i * 4f, 0f, 0f);
+            Vector3 position = offset + point;
+            Instantiate(turretPlacePrefab, position, Quaternion.identity);
         }
     }
 
