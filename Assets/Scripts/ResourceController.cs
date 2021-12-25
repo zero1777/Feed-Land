d4afb7b7d4026b781f70d592551ab3338998aecd
ms00769171 <ms00769171@gmail.com>
2021-12-25 12:47:40 +0800

    feat: add resource script to resource's prefabs

---
+++ Assets/Scripts/ResourceController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //for NavMeshAgent
+
+public class ResourceController : MonoBehaviour
+{
+
+    public int maxHealth = 3;
+    public int currentHealth;
+
+    public enum Ingredient
+    {
+        // ...
+    }
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+
+    void Update()
+    {
+        // debug health X to -hp
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            getHit(1);
+        }
+    }
+
+    public bool getHit(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            DestroyResource();
+            return true;
+        }
+        return false;
+    }
+
+    public void DestroyResource()
+    {
+        Destroy(gameObject);
+    }
+}
