623e063f9cbc83afe92046fe576977ca22f8abfb
LingSung <tumi9325@gmail.com>
2021-12-26 22:04:33 +0800

    feat: finish cannon-canvas

---
+++ Assets/Scripts/CannonCavas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonCavas : MonoBehaviour
+{
+    public Transform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// LateUpdate is called every frame, if the Behaviour is enabled.
+    /// It is called after all Update functions have been called.
+    /// </summary>
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
